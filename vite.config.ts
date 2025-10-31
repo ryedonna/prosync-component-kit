53d1e5ca6cc0633c8abcb8e7c1cc6c8690faf436
Ryedonna <53397953+ryedonna@users.noreply.github.com>
2025-10-31 13:34:28 +0800

    update vite configuration and dist folder

--- vite.config.ts
+++ vite.config.ts
@@ -10,14 +10,38 @@ export default defineConfig({
     lib: {
       entry: path.resolve(__dirname, 'src/index.ts'),
       name: 'ProsyncComponentsKit',
+      fileName: (format) => {
+        if (format === 'es') return 'prosync-components.es.js'
+        if (format === 'cjs') return 'prosync-components.cjs.js'
+        return 'prosync-components.umd.js'
+      },
     },
     rollupOptions: {
       external: ['vue'],
-      output: {
-        globals: {
-          vue: 'Vue',
+      output: [
+        {
+          format: 'es',
+          entryFileNames: 'prosync-components.es.js',
+          globals: {
+            vue: 'Vue',
+          },
         },
-      },
+        {
+          format: 'cjs',
+          entryFileNames: 'prosync-components.cjs.js',
+          globals: {
+            vue: 'Vue',
+          },
+        },
+        {
+          format: 'umd',
+          name: 'ProsyncComponentsKit',
+          entryFileNames: 'prosync-components.umd.js',
+          globals: {
+            vue: 'Vue',
+          },
+        },
+      ],
     },
   },
 })
