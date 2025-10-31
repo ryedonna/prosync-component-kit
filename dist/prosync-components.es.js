bf159ef38aa0ecbbb139333c472b3c569656b8bb
Ryedonna <53397953+ryedonna@users.noreply.github.com>
2025-10-31 14:10:37 +0800

    update to use tailwind instead of scss

--- dist/prosync-components.es.js
+++ dist/prosync-components.es.js
@@ -1,5 +1,5 @@
-import { defineComponent as o, createElementBlock as a, openBlock as r, normalizeClass as m, renderSlot as l, createTextVNode as c, toDisplayString as i } from "vue";
-const p = ["disabled"], f = /* @__PURE__ */ o({
+import { defineComponent as a, computed as l, createElementBlock as c, openBlock as i, normalizeClass as p, renderSlot as u, createTextVNode as d, toDisplayString as g } from "vue";
+const m = ["disabled"], v = /* @__PURE__ */ a({
   __name: "ProButton",
   props: {
     text: { type: String, required: !1 },
@@ -15,40 +15,73 @@ const p = ["disabled"], f = /* @__PURE__ */ o({
   },
   emits: ["click"],
   setup(e) {
-    return o({
+    a({
       name: "ProButton"
-    }), (t, n) => (r(), a("button", {
-      onClick: n[0] || (n[0] = (s) => t.$emit("click")),
+    });
+    const t = e, r = l(() => {
+      switch (t.size) {
+        case "sm":
+          return "px-3 py-1 text-sm";
+        case "lg":
+          return "px-6 py-3 text-lg";
+        case "md":
+        default:
+          return "px-4 py-2 text-base";
+      }
+    }), n = l(() => {
+      switch (t.theme) {
+        case "primary":
+          return "bg-blue-600 text-white hover:bg-blue-700";
+        case "secondary":
+          return "bg-gray-300 text-gray-800 hover:bg-gray-400";
+        case "success":
+          return "bg-green-500 text-white hover:bg-green-600";
+        case "warning":
+          return "bg-orange-600 text-white hover:bg-orange-700";
+        case "danger":
+          return "bg-red-600 text-white hover:bg-red-700";
+        case "default":
+        default:
+          return "bg-gray-100 text-gray-800 hover:bg-gray-200";
+      }
+    });
+    return (s, o) => (i(), c("button", {
+      onClick: o[0] || (o[0] = (y) => s.$emit("click")),
       disabled: e.disabled,
-      class: m(["jet-btn", `jet-btn-${e.theme}`])
+      class: p([
+        "inline-flex rounded border-none outline-none shadow-none",
+        r.value,
+        n.value,
+        e.disabled && "opacity-50 cursor-not-allowed"
+      ])
     }, [
-      l(t.$slots, "default", {}, () => [
-        c(i(e.text), 1)
-      ], !0)
-    ], 10, p));
+      u(s.$slots, "default", {}, () => [
+        d(g(e.text), 1)
+      ])
+    ], 10, m));
   }
-}), d = (e, t) => {
-  const n = e.__vccOpts || e;
-  for (const [s, u] of t)
-    n[s] = u;
-  return n;
-}, k = /* @__PURE__ */ d(f, [["__scopeId", "data-v-66045a0a"]]), _ = { class: "template" }, x = /* @__PURE__ */ o({
+}), b = { class: "template" }, x = /* @__PURE__ */ a({
   __name: "Template",
   props: {
     text: { type: String, required: !1 }
   },
   emits: ["click"],
   setup(e) {
-    return o({
+    return a({
       name: "Template"
-    }), (t, n) => (r(), a("div", _, [
-      l(t.$slots, "default", {}, () => [
-        c(i(e.text || "No results xxxx"), 1)
+    }), (t, r) => (i(), c("div", b, [
+      u(t.$slots, "default", {}, () => [
+        d(g(e.text || "No results xxxx"), 1)
       ], !0)
     ]));
   }
-}), y = /* @__PURE__ */ d(x, [["__scopeId", "data-v-bc3be847"]]);
+}), f = (e, t) => {
+  const r = e.__vccOpts || e;
+  for (const [n, s] of t)
+    r[n] = s;
+  return r;
+}, _ = /* @__PURE__ */ f(x, [["__scopeId", "data-v-bc3be847"]]);
 export {
-  k as ProButton,
-  y as Template
+  v as ProButton,
+  _ as Template
 };
--- src/components/button/ProButton.vue
+++ src/components/button/ProButton.vue
@@ -1,16 +1,26 @@
 <template>
-  <button @click="$emit('click')" :disabled="disabled" class="jet-btn" :class="`jet-btn-${theme}`">
-    <slot name="default">{{text}}</slot>
+  <button 
+    @click="$emit('click')" 
+    :disabled="disabled" 
+    :class="[
+      'inline-flex rounded border-none outline-none shadow-none',
+      sizeClasses,
+      themeClasses,
+      disabled && 'opacity-50 cursor-not-allowed'
+    ]"
+  >
+    <slot name="default">{{ text }}</slot>
   </button>
 </template>
 
 <script setup lang="ts">
-import {defineComponent} from "vue";
+import { defineComponent, computed } from "vue";
 
 defineComponent({
   name: "ProButton",
 });
-defineProps({
+
+const props = defineProps({
   text: { type: String, required: false },
   disabled: { type: Boolean, default: false },
   size: {
@@ -21,10 +31,28 @@ defineProps({
     type: String as () => 'primary' | 'secondary' | 'success' | 'warning' | 'danger' | 'default',
     default: 'default'
   }
-})
-defineEmits(["click"]);
-</script>
+});
 
-<style lang="scss" scoped>
-  @import './ProButton.scss';
-</style>
+defineEmits(["click"]);
+
+const sizeClasses = computed(() => {
+  switch(props.size) {
+    case 'sm': return 'px-3 py-1 text-sm';
+    case 'lg': return 'px-6 py-3 text-lg';
+    case 'md':
+    default: return 'px-4 py-2 text-base';
+  }
+});
+
+const themeClasses = computed(() => {
+  switch(props.theme) {
+    case 'primary': return 'bg-blue-600 text-white hover:bg-blue-700';
+    case 'secondary': return 'bg-gray-300 text-gray-800 hover:bg-gray-400';
+    case 'success': return 'bg-green-500 text-white hover:bg-green-600';
+    case 'warning': return 'bg-orange-600 text-white hover:bg-orange-700';
+    case 'danger': return 'bg-red-600 text-white hover:bg-red-700';
+    case 'default':
+    default: return 'bg-gray-100 text-gray-800 hover:bg-gray-200';
+  }
+});
+</script>
